bb24c42d19b4e30047b4fe44e7742a50dd9e8902
yotamloe <yotam12341@gmail.com>
2020-10-01 17:54:45 +0200

    adding script and configuration files

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this script will remove minikube and reinstall it on your machine
+minikube stop; minikube delete
+brew install minikube
+brew install helm
+minikube start --driver=docker
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo update
+helm install prometheus-operator prometheus-community/kube-prometheus-stack -f helm_vals.yaml
+sleep 240
+kubectl port-forward -n default prometheus-prometheus-operator-kube-p-prometheus-0 9090 &
+grafana=$(kubectl get pods --namespace=default -o jsonpath='{.items[1].metadata.name}')
+kubectl port-forward -n default ${grafana} 3000
+
+# username: admin
+# password: prom-operator
